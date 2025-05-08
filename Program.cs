fc01fdb7ec99500667d5807a0e11b2fc09db4844
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-08 16:19:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int[] nums = {4,1,2,1,2};
+Console.WriteLine(UniqueElement(nums));
+int UniqueElement(int[] nums)
+{
+    Dictionary<int, int> freq = new Dictionary<int, int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (freq.ContainsKey(nums[i]))
+        {
+            freq[nums[i]]++;
+        }
+        else
+        {
+            freq[nums[i]] = 1;
+        }
+    }
+    foreach (var pair in freq)
+    {
+        if (pair.Value == 1)
+        {
+            return pair.Key;
+        }
+    }
+    return -1;
+}
